ba7c727fa9e8dd0162bb475c74c37ac2a76210e5
Santiago Alvarez <santiagoalvarez9617@gmail.com>
2023-03-14 17:52:38 -0500

    Class 10: Implement lazy loading with useEffect

--- src/components/RandomFox.tsx
+++ src/components/RandomFox.tsx
@@ -1,13 +1,28 @@
 /* eslint-disable @next/next/no-img-element */
-import React, { useRef } from 'react'
+import React, { useRef, useEffect, useState } from 'react'
 
 export default function RandomFox(props: { image: string }) {
   
   const node = useRef<HTMLImageElement>(null)
 
-  node.current?.src
+  const [src, setSrc] = useState("data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMzIwIiBoZWlnaHQ9IjMyMCIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB2ZXJzaW9uPSIxLjEiLz4=")
+
+  useEffect(() => {
+    const intersectionObserver = new IntersectionObserver(entries => {
+      console.log(entries)
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          setSrc(props.image)
+        }
+      })
+    })
+    
+    node.current ? intersectionObserver.observe(node.current) : ''
+
+    return () => { intersectionObserver.disconnect() }
+  }, [props.image])
 
   return (
-    <img ref={node} src={props.image} alt="a foxy fox" width={320} />
+    <img ref={node} src={src} alt="a foxy fox" width={320} />
   )
 }
